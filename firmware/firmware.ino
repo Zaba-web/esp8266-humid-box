0e5866c5e648789dc3fad751a0eedccbf4c61b4b
Andriy Sotnyk <lodochnikkot@gmail.com>
2025-01-01 19:09:39 +0200

    Fan control

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -6,11 +6,18 @@
 ESP8266WebServer server(80);
 GY21 sensor;
 
-String html = "<!doctype html><html lang='en'><head><meta charset='UTF-8' /><meta name='viewport' content='width=device-width, initial-scale=1.0' /><title>Smart Tea Box</title><script type='module' crossorigin src='https://zaba-web.github.io/esp8266-humid-box/assets/index-C80MkkPp.js'></script><link rel='stylesheet' crossorigin href='https://zaba-web.github.io/esp8266-humid-box/assets/index-BJnS3woK.css'></head><body><div id='app'></div></body></html>";
+String frontendLoader = "<!doctype html><html lang='en'><head><meta charset='UTF-8' /><meta name='viewport' content='width=device-width, initial-scale=1.0' /><title>Smart Tea Box</title><script type='module' crossorigin src='https://zaba-web.github.io/esp8266-humid-box/assets/index-Dqn3vST0.js'></script><link rel='stylesheet' crossorigin href='https://zaba-web.github.io/esp8266-humid-box/assets/index-BJnS3woK.css'></head><body><div id='app'></div></body></html>";
 String boxName = "SHENG PUERH";
+
 const char* WIFI_SSID = "TOTOLINK_151";
 const char* PASSWORD = "m1n2b3v4";
 const int FAN_PIN = 14;
+const int VENTILATION_TIME = 30 * 1000;
+
+unsigned long lastTime = 0;
+unsigned long enabledTime = 0;
+
+bool fanEnabled = false;
 
 void enableFan() {
   digitalWrite(FAN_PIN, HIGH);
@@ -44,7 +51,7 @@ void wifiConnect() {
 
 void setupRoot() {
   server.on("/", [](){
-    server.send(200, "text/html", html);
+    server.send(200, "text/html", frontendLoader);
   });
   server.on("/name", []() {
     server.send(200, "text/html", boxName);
@@ -73,9 +80,34 @@ void setup(void){
 
   // SETUP FAN RELAY
   pinMode(FAN_PIN, OUTPUT);
+
+
+  // TEST FAN
+  enableFan();
+  delay(2000);
+  disableFan();
 }
  
 void loop(void)
 {
   server.handleClient();
+  handleVentilation();
+}
+
+void handleVentilation() {
+  if (fanEnabled) {
+    if (millis() >= enabledTime + VENTILATION_TIME) {
+      fanEnabled = false;
+      disableFan();
+    }
+    
+    return;
+  }
+  
+  if (millis() >= lastTime + (unsigned long) (30 * 60) * 1000) {
+    lastTime = millis();
+    enableFan();
+    fanEnabled = true;
+    enabledTime = lastTime;
+  }
 }
